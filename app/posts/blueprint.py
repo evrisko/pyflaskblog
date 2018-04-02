2bc6eac21126a5820b6a2143f86cdb7b318df4bb
Oleg Boslovyak <euvrisko@gmail.com>
2018-04-02 14:22:31 +0300

    Form search and form add post

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,19 +1,49 @@
 from flask import Blueprint
 from flask import render_template
 from models import Post, Tag
+from flask import request
+from .forms import PostForm
+from app import db
+from flask import redirect
+from flask import url_for
+
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
+
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('Something wrong')
+        return redirect(url_for('posts.index'))
+
+    form = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
+
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+    q = request.args.get('q')
+    if q:
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+    else:
+        posts = Post.query.order_by(Post.create.desc())
     return render_template('posts/index.html', posts=posts)
+
 
 @posts.route('/<slug>')
 def post_detail(slug):
     post = Post.query.filter(Post.slug==slug).first()
     tags = post.tags
     return render_template('posts/post_detail.html',post=post, tags=tags)
+
 
 @posts.route('/tag/<slug>')
 def tag_detail(slug):
