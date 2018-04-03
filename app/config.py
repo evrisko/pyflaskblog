79e9659ec98025332dbf33d598b01d90a183fce5
Oleg Boslovyak <euvrisko@gmail.com>
2018-04-03 14:01:10 +0300

    Add admin authorization for add/edit post

--- app/config.py
+++ app/config.py
@@ -3,3 +3,7 @@ class Configuration(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:12345678@localhost/blog'
     SECRET_KEY = 'something very secret key'
+
+# Flask security
+    SECURITY_PASSWORD_SALT = 'slat'
+    SECURITY_PASSWORD_HASH = 'bcrypt' #sha512_crypt
